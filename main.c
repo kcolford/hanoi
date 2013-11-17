59a12da109159f7b83c901cbab78be2970b98d0c
Kieran Colford <colfordk@gmail.com>
2013-11-17 13:26:56 -0500

    Set up the project so that it would use gnulib.

--- main.c
+++ main.c
@@ -22,32 +22,39 @@
 
 #include "config.h"
 #include "hanoi.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <argp.h>
 
-#ifndef INITIALMESSAGE
-# define INITIALMESSAGE "Move the top disk of tower %d to the top of tower %d."
-#endif
+#define INITIALMESSAGE "Move the top disk of tower %d to the top of tower %d."
+
+const char *argp_program_version = PACKAGE_STRING;
+const char *argp_program_bug_address = PACKAGE_BUGREPORT;
 
 static char *message = INITIALMESSAGE "\n";
 static int print_moves = 0;
 static int print_instruct = 1;
+static int n = 8;
+static int k = 2;
 
 /* The following is basically all just code relating to argument
    processing.  You can generally ignore everything that follows. */
 
-#if HAVE_ARGP_H && HAVE_STDLIB_H /* We need these to process the
-				    arguments. */
+#define HELP								\
+  "Print the solution the the Towers of Hanoi puzzle for ARG1 disks "	\
+  "(default 8) and ARG2 additional towers (towers excluding the "	\
+  "first and last) (default 2)."					\
 
-# include <stdlib.h> /* atoi */
-/* Non-Standard header for option processing. */
-# include <argp.h>
+static const struct argp_option opts[] = {
+  { "printf", 'p', "STRING", 0, "use STRING as the format string" },
+  { "silent", 's', 0,        0, "don't print out instructions"    },
+  { "moves",  'm', 0,        0, "print out the number of moves"   },
+  { 0 }
+};
 
-const char *argp_program_version = PACKAGE_STRING;
-const char *argp_program_bug_address = "<" PACKAGE_BUGREPORT ">";
-
-static error_t 
+error_t
 parse (int key, char *arg, struct argp_state *state)
 {
-  int *input = state->input;
   switch (key)
     {
     case 'p':
@@ -60,41 +67,18 @@ parse (int key, char *arg, struct argp_state *state)
       print_moves = 1;
       break;
     case ARGP_KEY_ARG:
-      if (state->arg_num > 2)
-	argp_usage (state);
+      if (state->arg_num == 0)
+	n = atoi (arg);
+      else if (state->arg_num == 1)
+	k = atoi (arg);
       else
-	input[state->arg_num] = atoi (arg);
+	return ARGP_ERR_UNKNOWN;
       break;
     default:
       return ARGP_ERR_UNKNOWN;
     }
   return 0;
 }
-
-static struct argp_option opts[] = {
-  { "printf", 'p', "STRING", 0, "Use STRING as the template "
-    "argument to printf.  The default arguement is \"" INITIALMESSAGE "\"" },
-  { "silent", 's', 0, 0, "Don't print solving instructions." },
-  { "moves", 'm', 0, 0, "Print the number of moves it takes to solve "
-    "the puzzle." },
-  { 0 }
-};
-
-static struct argp argp = { 
-  opts, parse, "[ARG1 [ARG2]]", 
-  "Print the solution to the Towers of Hanoi puzzle for ARG1 disks "
-  "(default 8) and ARG2 additional towers (default 2).\n"
-  "So the total number of towers is ARG2 + 2.\v"
-  "Project home page: " PACKAGE_URL
-};
-
-#else /* HAVE_ARGP_H && HAVE_STDLIB_H */
-
-/* If we don't have access to argp.h, then we have to hide the
-   definition of argp_parse using C99 variadic macros. */
-# define argp_parse(...) (0)
-
-#endif /* HAVE_ARGP_H && HAVE_STDLIB_H */
 
 static void
 print_instructions (int start, int end)
@@ -104,10 +88,10 @@ print_instructions (int start, int end)
 
 int main (int argc, char *argv[])
 {
-  int args[] = { 8, 2 };
   int moves;
-  argp_parse (&argp, argc, argv, 0, 0, args);
-  moves = han_tower (args[0], args[1], print_instructions);
+  struct argp argument_parser = { opts, parse, "[ARG1 [ARG2]]", HELP };
+  argp_parse (&argument_parser, argc, argv, 0, 0, 0);
+  moves = han_tower (n, k, print_instructions);
   if (print_moves)
     printf ("%d\n", moves);
   return 0;
